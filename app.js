1273cb99763922f3c60b34c6042536bf5053d156
vnlemanhthanh <vnlemanhthanh@gmail.com>
2024-10-24 01:04:53 +0700

    up

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const fs = require('fs')
+// const fs = require('fs')
 
-fs.writeFileSync('note.txt', 'Hello World')
+// fs.writeFileSync('note.txt', 'Hello World.')
+
+// fs.appendFileSync('note.txt', ' My name is vnlemanhthanh', '')
+
+// const name = require('./utils')
+
+// console.log(name)
+
+const validator = require('validator')
+const getNotes = require('./note.js')
+
+const msg = getNotes()
+
+console.log(msg)
+console.log(validator.isEmail('vnlemanhthanh@gmail.com'))
+console.log(validator.isURL('https://vnlemanhthanh.github/'))
